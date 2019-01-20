9ece61ac9116dab621736718e6a7a254338c649d
skatpgusskat <skatpgusskat@naver.com>
2019-01-21 01:01:01 +0900

    Measure text width and height by setting content

--- src/Text.ts
+++ src/Text.ts
@@ -9,9 +9,15 @@ export default class Text extends Sprite {
     private fontFamily: string = 'Arial',
   ) {
     super(location, new Vector(0, 0)); // TODO : change new Vector(0, 0), it is temp.
+    this.updateSizeWithContent();
   }
   public setContent(content: string) {
     this.content = content;
+    this.updateSizeWithContent();
+  }
+  private updateSizeWithContent() {
+    const textMatrics = this.ctx.measureText(this.content);
+    this.size = new Vector(textMatrics.width, this.fontSize);
   }
   protected draw(): void {
     this.ctx.textAlign = 'center';
